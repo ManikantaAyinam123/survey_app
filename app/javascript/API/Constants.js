5e39b1d4e5d42188df7be33f2eb65662bdac903c
ManikantaAyinam123 <manikanta@actimize.in>
2024-07-12 12:38:35 +0530

    migrate

--- app/javascript/API/Constants.js
+++ app/javascript/API/Constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://157.245.102.123:5000/api/"; 
+export const BASE_URL = "http://localhost:3000/api/"; 
 export const STATUS_CODE = {
   INTERNAL_SERVER_ERROR: 500,
   SUCCESS: 200,
